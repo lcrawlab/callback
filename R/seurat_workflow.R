16ee319c56c68f17beb041ea5e91d9741e25ece8
agdenadel <agdenadel@gmail.com>
2023-05-02 14:02:29 -0700

    Add seurat function skeletons

---
+++ R/seurat_workflow.R
@@ -0,0 +1,20 @@
+seurat_workflow <- function(seurat_obj, num_variable_features) {
+}
+
+compare_clusterings <- function(seurat_obj1, seurat_obj2) {
+}
+
+compute_knockoff_filter_one_cluster <- function(seurat_obj, cluster, q) {
+}
+
+compute_knockoff_filter <- function(seurat_obj, cluster1, cluster2, q) {
+
+}
+
+FindMarkersWithKnockoffs <- function(seurat_obj, ident.1, ident.2, q, num_var_features) {
+}
+
+kfwer_knockoff_threshold <- function(W, k, fwer) {
+
+}
+
