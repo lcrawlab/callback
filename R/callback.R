eebe8bca2cc0c377305b431f8aed7e4cc1e271c3
agdenadel <agdenadel@gmail.com>
2024-02-12 15:41:06 -0800

    linting

--- R/callback.R
+++ R/callback.R
@@ -28,21 +28,20 @@
 #' @name FindClustersCallback
 #' @export
 FindClustersCallback <- function(seurat_obj,
-                            resolution_start = 0.8,
-                            reduction_percentage = 0.2,
-                            num_clusters_start = 20,
-                            dims = 1:10,
-                            algorithm = "louvain", # todo implement all algos
-                            assay = "RNA",
-                            cores = 1,
-                            verbose = TRUE) {
+                                 resolution_start = 0.8,
+                                 reduction_percentage = 0.2,
+                                 num_clusters_start = 20,
+                                 dims = 1:10,
+                                 algorithm = "louvain", # todo implement all algos
+                                 assay = "RNA",
+                                 cores = 1,
+                                 verbose = TRUE) {
 
   # todo check function arguments for validity
-  
 
-  knockoff_seurat_obj <- get_seurat_obj_with_knockoffs(seurat_obj, assay=assay)
+  knockoff_seurat_obj <- get_seurat_obj_with_knockoffs(seurat_obj, assay = assay)
 
-  num_variable_features <- 2*length(Seurat::VariableFeatures(seurat_obj))
+  num_variable_features <- 2 * length(Seurat::VariableFeatures(seurat_obj))
 
 
   # Pre-process data
@@ -76,7 +75,7 @@ FindClustersCallback <- function(seurat_obj,
 
   resolution_param <- resolution_start
 
-  while(TRUE) {
+  while (TRUE) {
     if (verbose) {
       message("####################################################################")
       message(paste("Finding clusters with", stringr::str_to_title(algorithm), "algorithm"))
@@ -93,13 +92,13 @@ FindClustersCallback <- function(seurat_obj,
       #plan("sequential") # todo log number of cores being used # this is a weird one because leiden has a forked job hanging
       knockoff_seurat_obj <- Seurat::FindClusters(knockoff_seurat_obj,
                                                   resolution = resolution_param,
-                                                  algorithm=4,
+                                                  algorithm = 4,
                                                   method = "igraph",
                                                   verbose = FALSE)
     }
 
     # Reduce resolution for next iteration of the loop
-    resolution_param <- (1 -reduction_percentage) * resolution_param
+    resolution_param <- (1 - reduction_percentage) * resolution_param
 
     k <- length(levels(Seurat::Idents(knockoff_seurat_obj)))
     #knock_idents <- 0:(k-1)
@@ -111,57 +110,60 @@ FindClustersCallback <- function(seurat_obj,
 
     knock_idents <- levels(Seurat::Idents(knockoff_seurat_obj))
 
-
-    num_selected_matrix <- matrix(nrow=k, ncol=k)
+    num_selected_matrix <- matrix(nrow = k, ncol = k)
 
     found_no_sign_diff <- FALSE
-    
+
+    num_clusters <- length(knock_idents)
+
 
     if (verbose) {
+      progress_bar_length <- num_clusters * (num_clusters - 1) / 2
       cli::cli_progress_bar("Processing cluster pairs:",
-                            total = (length(knock_idents) * (length(knock_idents) - 1) / 2),
+                            total = progress_bar_length,
                             clear = FALSE)
     }
 
     m <- 0
-    for (i in 1:length(knock_idents)) {
-      for (j in 1:length(knock_idents)) {
+    for (i in 1:num_clusters) {
+      for (j in 1:num_clusters) {
         if (j >= i) {
           next
         }
-        
+
         m <- m + 1
-        
+
         if (verbose) {
           cli::cli_progress_update()
         }
-        
-        markers.selected <- compute_knockoff_filter(knockoff_seurat_obj,
+
+        markers_selected <- compute_knockoff_filter(knockoff_seurat_obj,
                                                     knock_idents[i],
                                                     knock_idents[j],
                                                     0.05, # todo add var names?
-                                                    num_cores=cores)
-        
-        num.selected <- nrow(markers.selected$selected_features)
-        
-        if (num.selected == 0) {
+                                                    num_cores = cores)
+
+        num_selected <- nrow(markers_selected$selected_features)
+
+        if (num_selected == 0) {
           found_no_sign_diff <- TRUE
           break
         }
 
-        num_selected_matrix[i,j] <- num.selected
-        num_selected_matrix[j,i] <- num.selected
+        num_selected_matrix[i, j] <- num_selected
+        num_selected_matrix[j, i] <- num_selected
 
       }
       if (found_no_sign_diff) {
         if (verbose) {
           cli::cli_progress_done()
-          message("Found clusters with no significant differences.\nProgressing to next clustering iteration.")
+          message("Found clusters with no significant differences.")
+          message("Progressing to next clustering iteration.")
         }
         break
       }
     }
-    
+
     if (found_no_sign_diff) {
       next
     }
@@ -209,9 +211,10 @@ get_seurat_obj_with_knockoffs <- function(seurat_obj, assay = "RNA") {
 
   message("Computing knockoffs")
   ko <- as.data.frame(lapply(ml_estimates,
-                             function(x) { rzipoisson(nrow(seurat_obj_data),
-                                                    x$lambda.hat,
-                                                    x$pi.hat)
+                             function(x) {
+                                           rzipoisson(nrow(seurat_obj_data),
+                                           x$lambda.hat,
+                                           x$pi.hat)
                              }))
 
 
